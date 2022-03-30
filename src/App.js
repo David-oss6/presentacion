41fd958340355948888d9368db80786daaadb6b3
David <im.david85@gmail.com>
2022-03-30 17:04:04 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -82,6 +82,7 @@ function App() {
                   <p>Barcelona</p>
                 </div>
               </div>
+              <h1>Miwerda</h1>
             </TitleContainer>
             {!inicio && modal && <DropDown />}
             {!inicio && !modal && (
